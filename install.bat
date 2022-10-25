8dafc595071658f2ee0c732e473ced8d105e6b48
Daeloth <37391512+Daeloth@users.noreply.github.com>
2022-10-25 04:16:24 -0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,7 +1,7 @@
 git clone https://github.com/Maekir7/RegistrAPP_Hernandez_Cortes_001D
 call npm i -g typescript @ionic/cli @ionic/storage-angular
 call ionic start nombre blank --type angular
-rd /s /q "/nombre/src"
+rd /s /q "./nombre/src"
 move "./RegistrAPP_Hernandez_Cortes_001D/src" "./nombre/src"
 rd /s /q "./RegistrAPP_Hernandez_Cortes_001D"
 cd "./nombre"
